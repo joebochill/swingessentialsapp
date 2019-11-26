0dd218ac0a3322ccb05f96874c86e11c3affbf2b
joebochill <boyle.p.joseph@gmail.com>
2019-11-25 21:17:38 -0500

    Add condensed font

--- src/navigation/screens/home/Home.tsx
+++ src/navigation/screens/home/Home.tsx
@@ -4,7 +4,7 @@ import { Icon } from 'react-native-elements';
 import { withNavigation } from 'react-navigation';
 import { Header } from '@pxblue/react-native-components';
 import { EmptyState, wrapIcon } from '@pxblue/react-native-components';
-import { tsPropertySignature } from '@babel/types';
+import topology from '../../../images/topology_40.png';
 // import * as Colors from '@pxblue/colors';
 const MenuIcon = wrapIcon({ IconClass: Icon, name: 'menu' });
 const AccessTime = wrapIcon({ IconClass: Icon, name: 'access-time' });
@@ -13,7 +13,11 @@ export const Home = withNavigation((props) => (
     <View style={styles.container}>
         <Header 
             navigation={{ icon: MenuIcon, onPress: () => props.navigation.openDrawer() }} 
-            title={'SWING ESSENTIALS'} />
+            backgroundImage={topology}
+            backgroundColor={'#4f4c81'}
+            title={'SWING ESSENTIALS'} 
+            subtitle={'A PGA Pro in your pocket'}
+        />
         <EmptyState IconClass={AccessTime} title={'Coming Soon'} />
     </View>
 ));
