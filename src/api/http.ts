8ca07e3a0cbdd53815103ba700e5af08faf434a3
joebochill <boyle.p.joseph@gmail.com>
2020-01-22 19:54:45 -0500

    Add network connection warning

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,6 +1,9 @@
 import { TOKEN } from './tokenMiddleware';
 import { BASEURL, AUTH } from '../constants';
 import { Logger } from '../utilities/logging';
+import NetInfo from "@react-native-community/netinfo";
+import { Alert } from 'react-native';
+
 
 export enum HttpMethod {
     POST = 'POST',
@@ -81,6 +84,12 @@ export class HttpRequest<TResponses extends GeneralResponseMapping = {}> {
                 }
             })
             .catch(error => {
+                NetInfo.fetch()
+                    .then(state => {
+                        if (!state.isConnected) {
+                            Alert.alert('It looks like you are having trouble with your network connection. Make sure you have a reliable internet connection before interacting with the app.')
+                        }
+                    });
                 Logger.logError({
                     code: 'HTP100',
                     description: `Fetch call failed for ${this.endpoint}.`,
@@ -119,6 +128,12 @@ export class HttpRequest<TResponses extends GeneralResponseMapping = {}> {
                 }
             })
             .catch(error => {
+                NetInfo.fetch()
+                    .then(state => {
+                        if (!state.isConnected) {
+                            Alert.alert('It looks like you are having trouble with your network connection. Make sure you have a reliable internet connection before interacting with the app.')
+                        }
+                    });
                 Logger.logError({
                     code: 'HTP200',
                     description: `XHR call failed for ${this.endpoint}.`,
