6cece784ac4bc89e7933ea9df9e8b2bd423b9dee
joebochill <boyle.p.joseph@gmail.com>
2025-04-11 08:40:34 -0400

    feat: enable the youtube controls options for videos

--- src/components/videos/YoutubeCard.tsx
+++ src/components/videos/YoutubeCard.tsx
@@ -1,11 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { ActivityIndicator } from 'react-native-paper';
+import DeviceInfo from 'react-native-device-info';
 
 // Components
-import { View, TouchableOpacity, ViewProps } from 'react-native';
-import { Stack, Typography } from '..';
-import YoutubePlayer from 'react-native-youtube-iframe';
+import { View, TouchableOpacity, ViewProps, Platform } from 'react-native';
+import { Paragraph, Stack, Typography } from '..';
+import YoutubePlayer, { YoutubeIframeRef } from 'react-native-youtube-iframe';
 import MatIcon from 'react-native-vector-icons/MaterialIcons';
+import Modal from 'react-native-modal';
+
 // Styles
 import { width as deviceWidth, aspectHeight } from '../../utilities/dimensions';
 import { useAppTheme } from '../../theme';
@@ -67,6 +70,10 @@ export const YoutubeCard: React.FC<YoutubeCardProps> = (props) => {
     const { videoWidth = width, headerTitle, headerSubtitle, video, onExpand, style } = props;
     const [videoReady, setVideoReady] = useState(false);
     const [playing, setPlaying] = useState(false);
+    const [showSettingsWarning, setShowSettingsWarning] = useState(false);
+    const [hasPlayed, setHasPlayed] = useState(false);
+
+    const playerRef = useRef<YoutubeIframeRef>(null);
 
     const onStateChange = useCallback((state: string) => {
         switch (state) {
@@ -76,6 +83,7 @@ export const YoutubeCard: React.FC<YoutubeCardProps> = (props) => {
                 break;
             case 'playing':
                 setPlaying(true);
+                setHasPlayed(true);
                 break;
             default:
                 return;
@@ -83,73 +91,145 @@ export const YoutubeCard: React.FC<YoutubeCardProps> = (props) => {
     }, []);
 
     return (
-        <View
-            style={[
-                {
-                    shadowColor: '#000',
-                    shadowOpacity: 0.4,
-                    shadowRadius: 3,
-                    shadowOffset: {
-                        width: 0,
-                        height: 1,
+        <>
+            <View
+                style={[
+                    {
+                        shadowColor: '#000',
+                        shadowOpacity: 0.4,
+                        shadowRadius: 3,
+                        shadowOffset: {
+                            width: 0,
+                            height: 1,
+                        },
+                        elevation: 1,
+                        flex: 1,
+                        backgroundColor: theme.colors.surface,
+                        borderRadius: theme.roundness,
                     },
-                    elevation: 1,
-                    flex: 1,
-                    backgroundColor: theme.colors.surface,
-                    borderRadius: theme.roundness,
-                },
-                ...(Array.isArray(style) ? style : [style]),
-            ]}
-        >
-            {/* outer shadow gets clipped without this inner view */}
-            <Stack
-                style={{
-                    borderRadius: theme.roundness,
-                    overflow: 'hidden',
-                }}
+                    ...(Array.isArray(style) ? style : [style]),
+                ]}
             >
-                {(headerTitle || headerSubtitle || onExpand) && (
-                    <YoutubeCardHeader title={headerTitle} subtitle={headerSubtitle} onExpand={onExpand} />
-                )}
-                <TouchableOpacity
-                    activeOpacity={1}
-                    onPress={() => {
-                        setPlaying((p) => !p);
+                {/* outer shadow gets clipped without this inner view */}
+                <Stack
+                    style={{
+                        borderRadius: theme.roundness,
+                        overflow: 'hidden',
                     }}
                 >
-                    <View pointerEvents={'none'}>
-                        <YoutubePlayer
-                            height={aspectHeight(videoWidth)}
-                            width={videoWidth}
-                            play={playing}
-                            videoId={video}
-                            onChangeState={onStateChange}
-                            onReady={(): void => setVideoReady(true)}
-                            webViewProps={{
-                                androidLayerType: 'hardware',
+                    {(headerTitle || headerSubtitle || onExpand) && (
+                        <YoutubeCardHeader title={headerTitle} subtitle={headerSubtitle} onExpand={onExpand} />
+                    )}
+                    <YoutubePlayer
+                        ref={playerRef}
+                        height={aspectHeight(videoWidth)}
+                        width={videoWidth}
+                        play={playing}
+                        videoId={video}
+                        onChangeState={onStateChange}
+                        onReady={(): void => setVideoReady(true)}
+                        webViewProps={{
+                            androidLayerType: 'hardware',
+                        }}
+                        onError={(e): void => {
+                            void Logger.logError({
+                                code: 'YTB-001',
+                                description: `Youtube player encountered an error.`,
+                                rawErrorCode: '000',
+                                rawErrorMessage: e,
+                            });
+                        }}
+                        initialPlayerParams={{
+                            modestbranding: true,
+                            controls: true,
+                        }}
+                    />
+                    {!videoReady && (
+                        <ActivityIndicator
+                            size={theme.size.xl}
+                            style={{ position: 'absolute', height: '100%', width: '100%', top: 0, left: 0 }}
+                        />
+                    )}
+                    {/* Main Video Body Blocker (for Scroll) */}
+                    <TouchableOpacity
+                        onPress={() => {
+                            setPlaying((p) => !p);
+                        }}
+                        style={{
+                            position: 'absolute',
+                            top: theme.size.xl,
+                            left: 0,
+                            right: 0,
+                            bottom: 58,
+                            // backgroundColor: 'rgba(255,0,0,0.35)',
+                        }}
+                    ></TouchableOpacity>
+                    {/* CC And Settings Button Blocker */}
+                    {hasPlayed && (
+                        <TouchableOpacity
+                            activeOpacity={1}
+                            onPress={() => {
+                                if (hasPlayed) setShowSettingsWarning(true);
                             }}
-                            onError={(e): void => {
-                                void Logger.logError({
-                                    code: 'YTB-001',
-                                    description: `Youtube player encountered an error.`,
-                                    rawErrorCode: '000',
-                                    rawErrorMessage: e,
-                                });
-                            }}
-                            initialPlayerParams={{
-                                modestbranding: true,
-                                controls: false,
+                            style={{
+                                position: 'absolute',
+                                width: Platform.OS === 'ios' ? (DeviceInfo.isTablet() ? 80 : 60) : 90,
+                                right: Platform.OS === 'ios' && DeviceInfo.isTablet() ? 160 : 130,
+                                height: Platform.OS === 'ios' && DeviceInfo.isTablet() ? 35 : 40,
+                                bottom: 0,
+                                // backgroundColor: DeviceInfo.isTablet() ? 'rgba(0,255,0,0.35)' : 'rgba(0,0, 255,0.35)',
                             }}
                         />
-                        {!videoReady && (
-                            <ActivityIndicator
-                                size={theme.size.xl}
-                                style={{ position: 'absolute', height: '100%', width: '100%', top: 0, left: 0 }}
-                            />
-                        )}
-                    </View>
+                    )}
+                    {/* Airplay Settings Button Blocker */}
+                    {Platform.OS === 'ios' && DeviceInfo.isTablet() && hasPlayed && (
+                        <TouchableOpacity
+                            activeOpacity={1}
+                            onPress={() => {
+                                if (hasPlayed) setShowSettingsWarning(true);
+                            }}
+                            style={{
+                                position: 'absolute',
+                                width: 40,
+                                right: 50,
+                                height: 35,
+                                bottom: 0,
+                                // backgroundColor: 'rgba(255,0, 255,0.35)',
+                            }}
+                        />
+                    )}
+                </Stack>
+            </View>
+            <Modal
+                animationIn="slideInUp"
+                animationOut={'slideOutDown'}
+                backdropColor={'rgba(0,0,0,0.35)'}
+                onBackdropPress={(): void => {
+                    setShowSettingsWarning(false);
+                }}
+                onDismiss={(): void => {
+                    setShowSettingsWarning(false);
+                }}
+                isVisible={showSettingsWarning}
+            >
+                <TouchableOpacity
+                    activeOpacity={1}
+                    onPress={async () => {
+                        setShowSettingsWarning(false);
+                    }}
+                    style={[
+                        {
+                            borderRadius: theme.roundness,
+                            backgroundColor: '#1f1f1f',
+                            padding: theme.spacing.lg,
+                        },
+                    ]}
+                >
+                    <Paragraph style={{ color: 'white', textAlign: 'center' }}>
+                        To change video playback settings, open in full-screen mode.
+                    </Paragraph>
                 </TouchableOpacity>
-            </Stack>
-        </View>
+            </Modal>
+        </>
     );
 };
